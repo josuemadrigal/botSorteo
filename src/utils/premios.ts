2c2b6766f33fa2ce64178a9c89cd883ea74bd925
josuemadrigal <josuemadrigal12@gmail.com>
2025-05-30 14:42:24 -0400

    Update

--- src/utils/premios.ts
+++ src/utils/premios.ts
@@ -7,6 +7,36 @@ type PremiosList = {
 };
 
 export const premios: PremiosList = {
+  abanico: {
+    title: "un Abanico",
+    imgUrl:
+      "https://oechsle.vtexassets.com/arquivos/ids/18333322/2668525_1.jpg?v=638560911682030000",
+  },
+  "bono-de-rd2000-pesos": {
+    title: "un Bono de RD$2,000 pesos",
+    imgUrl:
+      "https://oechsle.vtexassets.com/arquivos/ids/18333322/2668525_1.jpg?v=638560911682030000",
+  },
+  "bono-de-rd1500-pesos": {
+    title: "un Bono de RD$1,500 pesos",
+    imgUrl:
+      "https://oechsle.vtexassets.com/arquivos/ids/18333322/2668525_1.jpg?v=638560911682030000",
+  },
+  "cilindro-de-gas-50lb": {
+    title: "un Cilindro de gas",
+    imgUrl:
+      "https://oechsle.vtexassets.com/arquivos/ids/18333322/2668525_1.jpg?v=638560911682030000",
+  },
+  "estufa-de-horno": {
+    title: "un Estufa de horno",
+    imgUrl:
+      "https://oechsle.vtexassets.com/arquivos/ids/18333322/2668525_1.jpg?v=638560911682030000",
+  },
+  "estufa-de-mesa-4-hornillas": {
+    title: "un Estufa mesa 4 hornillas",
+    imgUrl:
+      "https://oechsle.vtexassets.com/arquivos/ids/18333322/2668525_1.jpg?v=638560911682030000",
+  },
   "juego-de-ollas": {
     title: "un Juego de ollas",
     imgUrl:
@@ -17,8 +47,8 @@ export const premios: PremiosList = {
     imgUrl:
       "https://oechsle.vtexassets.com/arquivos/ids/18333322/2668525_1.jpg?v=638560911682030000",
   },
-  plancha: {
-    title: "una Plancha",
+  microondas: {
+    title: "una Microonda",
     imgUrl:
       "https://oechsle.vtexassets.com/arquivos/ids/18333322/2668525_1.jpg?v=638560911682030000",
   },
@@ -27,8 +57,8 @@ export const premios: PremiosList = {
     imgUrl:
       "https://oechsle.vtexassets.com/arquivos/ids/18333322/2668525_1.jpg?v=638560911682030000",
   },
-  prueba: {
-    title: "PREMIO TEST",
+  freidora: {
+    title: "un Freidora",
     imgUrl:
       "https://oechsle.vtexassets.com/arquivos/ids/18333322/2668525_1.jpg?v=638560911682030000",
   },
